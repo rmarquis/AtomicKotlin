c3025a4b39de482f4231f5cd40f598758e1f07a9
Remy Marquis <remy.marquis@gmail.com>
2021-06-02 18:08:11 +0200

    Constraining Visibility

--- Introduction to Objects/Constraining Visibility/Exercise 2/src/Task.kt
+++ Introduction to Objects/Constraining Visibility/Exercise 2/src/Task.kt
@@ -2,46 +2,46 @@
 package constrainingVisibilityExercise2
 
 class Robot(
-  val fieldSize: Int,
-  var x: Int,
-  var y: Int
+    private val fieldSize: Int,
+    private var x: Int,
+    private var y: Int
 ) {
-  fun crossBoundary(coordinate: Int): Int {
-    val inBounds = coordinate % fieldSize
-    return if (inBounds < 0) {
-      fieldSize + inBounds
-    } else {
-      inBounds
+    private fun crossBoundary(coordinate: Int): Int {
+        val inBounds = coordinate % fieldSize
+        return if (inBounds < 0) {
+            fieldSize + inBounds
+        } else {
+            inBounds
+        }
     }
-  }
 
-  fun right(steps: Int) {
-    x += steps
-    x = crossBoundary(x)
-  }
+    fun right(steps: Int) {
+        x += steps
+        x = crossBoundary(x)
+    }
 
-  fun left(steps: Int) {
-    x -= steps
-    x = crossBoundary(x)
-  }
+    fun left(steps: Int) {
+        x -= steps
+        x = crossBoundary(x)
+    }
 
-  fun down(steps: Int) {
-    y += steps
-    y = crossBoundary(y)
-  }
+    fun down(steps: Int) {
+        y += steps
+        y = crossBoundary(y)
+    }
 
-  fun up(steps: Int) {
-    y -= steps
-    y = crossBoundary(y)
-  }
+    fun up(steps: Int) {
+        y -= steps
+        y = crossBoundary(y)
+    }
 
-  fun getLocation(): String = "($x, $y)"
+    fun getLocation(): String = "($x, $y)"
 
-  override fun toString() = "Robot(x=$x, y=$y)"
+    override fun toString() = "Robot(x=$x, y=$y)"
 }
 
 fun main() {
-  val robot = Robot(10, 1, 1)
-  // Should be "can't access x" error:
+    val robot = Robot(10, 1, 1)
+    // Should be "can't access x" error:
 //  println(robot.x)
 }
--- Introduction to Objects/Constraining Visibility/Exercise 5/src/Task.kt
+++ Introduction to Objects/Constraining Visibility/Exercise 5/src/Task.kt
@@ -2,24 +2,26 @@
 package constrainingVisibilityExercise5
 
 fun printSum(x: Int) {
-  if (x <= 0) {
-    println("Incorrect input: $x should be positive")
-    return
-  }
-  println((0..x).sum())
+    if (checkArgument(x)) return
+    println((0..x).sum())
 }
 
 fun printAverage(x: Int) {
-  if (x <= 0) {
-    println("Incorrect input: $x should be positive")
-    return
-  }
-  println((0..x).average())
+    if (checkArgument(x)) return
+    println((0..x).average())
+}
+
+private fun checkArgument(x: Int): Boolean {
+    if (x <= 0) {
+        println("Incorrect input: $x should be positive")
+        return true
+    }
+    return false
 }
 
 fun main() {
-  printSum(-10)
-  printAverage(-10)
+    printSum(-10)
+    printAverage(-10)
 }
 /* Output:
 Incorrect input: -10 should be positive
