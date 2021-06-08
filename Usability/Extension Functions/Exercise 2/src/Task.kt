bde989050ad38a38709b4e94c3bddfcd8778d5c0
Remy Marquis <remy.marquis@gmail.com>
2021-06-08 15:43:45 +0200

    Extension Functions

--- Usability/Extension Functions/Exercise 2/src/Task.kt
+++ Usability/Extension Functions/Exercise 2/src/Task.kt
@@ -1,13 +1,14 @@
 // ExtensionFunctions/Task2.kt
 package extensionFunctionsExercise2
+
 import atomictest.eq
 
-fun Int.isOdd(): Boolean = TODO()
+fun Int.isOdd(): Boolean = this % 2 == 1
 
-fun Int.isEven(): Boolean = TODO()
+fun Int.isEven(): Boolean = !isOdd()
 
 fun main() {
-  1.isOdd() eq true
-  2.isEven() eq true
-  13.isEven() eq false
+    1.isOdd() eq true
+    2.isEven() eq true
+    13.isEven() eq false
 }
