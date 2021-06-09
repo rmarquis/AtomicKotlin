ac03fbc639f1be85d513748c80ebb86328874647
Remy Marquis <remy.marquis@gmail.com>
2021-06-09 15:25:51 +0200

    when Expressions

--- Usability/when Expressions/Exercise 3/src/Task.kt
+++ Usability/when Expressions/Exercise 3/src/Task.kt
@@ -1,12 +1,26 @@
 // WhenExpressions/Task3.kt
 package whenExpressionsExercise3
+
 import atomictest.eq
 
 fun balanced(input: String): Boolean {
-  TODO()
+    var leftUnmatched = 0
+    for (c in input) {
+        when (c) {
+            '(' -> leftUnmatched++
+            ')' -> leftUnmatched--
+            ' ' -> {
+            }
+            else -> throw IllegalArgumentException(
+                "The string must consist only of parentheses and whitespaces"
+            )
+        }
+        if (leftUnmatched < 0) return false
+    }
+    return leftUnmatched == 0
 }
 
 fun main() {
-  balanced("(()) ()") eq true
-  balanced(")(") eq false
+    balanced("(()) ()") eq true
+    balanced(")(") eq false
 }
