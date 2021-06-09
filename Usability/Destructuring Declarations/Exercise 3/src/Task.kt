4d65a98057e4f38504dfb317beddd41f66fbf69f
Remy Marquis <remy.marquis@gmail.com>
2021-06-09 21:20:23 +0200

    Destructuring Declarations

--- Usability/Destructuring Declarations/Exercise 3/src/Task.kt
+++ Usability/Destructuring Declarations/Exercise 3/src/Task.kt
@@ -2,18 +2,19 @@
 package destructuringDeclarationsExercise3
 
 data class Person(
-  val name: String,
-  // TODO: add property here
-  val age: Int
+    val name: String,
+    val surname: String,
+    val age: Int
 )
 
 fun displayPersonInfo(person: Person) {
-  val (name, age) = person
-// TODO
+    val (name, age) = person
+    println("Name: $name")
+    println("Age: $age")
 }
 
 fun main() {
-val person = Person("Alice",  30)
-//  val person = Person("Alice", "Johnson", 30)
-  displayPersonInfo(person)
+    // val person = Person("Alice",  30)
+    val person = Person("Alice", "Johnson", 30)
+    displayPersonInfo(person)
 }
