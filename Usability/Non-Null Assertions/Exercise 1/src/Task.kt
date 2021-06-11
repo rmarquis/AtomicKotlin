1d980585f2b20592ed2232b1272eb9cee800ca5e
Remy Marquis <remy.marquis@gmail.com>
2021-06-11 18:23:06 +0200

    Non-Null Assertions

--- Usability/Non-Null Assertions/Exercise 1/src/Task.kt
+++ Usability/Non-Null Assertions/Exercise 1/src/Task.kt
@@ -1,17 +1,18 @@
 // NonNullAssertions/Task1.kt
 package nonNullAssertionsExercise1
+
 import atomictest.*
 
-class Rocket
+class Rocket {
+    fun ignition() = "Liftoff!"
+}
 
-fun launch() {}
+fun launch(rocket: Rocket?) = rocket!!.ignition()
 
 fun main() {
-/*
-  val rocket = Rocket()
-  launch(rocket) eq "Liftoff!"
-  capture {
-    launch(null)
-  } eq "NullPointerException"
-*/
+    val rocket = Rocket()
+    launch(rocket) eq "Liftoff!"
+    capture {
+        launch(null)
+    } eq "NullPointerException"
 }
--- Usability/Non-Null Assertions/Exercise 3/src/Task.kt
+++ Usability/Non-Null Assertions/Exercise 3/src/Task.kt
@@ -1,14 +1,17 @@
 // NonNullAssertions/Task3.kt
 package nonNullAssertionsExercise3
+
 import atomictest.eq
 import kotlin.math.absoluteValue
 
 fun absMinIsMax(list: List<Int>): Boolean {
-  TODO()
+    if (list.isEmpty()) return false
+    return list.maxOrNull()!!.absoluteValue ==
+            list.minOrNull()!!.absoluteValue
 }
 
 fun main() {
-  absMinIsMax(listOf(-10, 1, 10)) eq true
-  absMinIsMax(listOf(10, 10)) eq true
-  absMinIsMax(listOf(-10, 1)) eq false
+    absMinIsMax(listOf(-10, 1, 10)) eq true
+    absMinIsMax(listOf(10, 10)) eq true
+    absMinIsMax(listOf(-10, 1)) eq false
 }
