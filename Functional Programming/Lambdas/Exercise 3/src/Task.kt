d70c2dad453d174daa73d676914572966f9fa3c0
Remy Marquis <remy.marquis@gmail.com>
2021-06-12 16:05:34 +0200

    Lambdas

--- Functional Programming/Lambdas/Exercise 3/src/Task.kt
+++ Functional Programming/Lambdas/Exercise 3/src/Task.kt
@@ -1,16 +1,17 @@
 // Lambdas/Task3.kt
 package lambdasExercise3
+
 import atomictest.eq
 
 data class Student(val id: Int, val name: String)
 
 fun registerStudents(names: List<String>, startId: Int = 0): List<Student> =
-  TODO()
+    names.mapIndexed { index, name -> Student(startId + index, name) }
 
 fun main() {
-  val students = listOf("Alice", "Bob")
-  registerStudents(students) eq
-    listOf(Student(0, "Alice"), Student(1, "Bob"))
-  registerStudents(students, startId = 10) eq
-    listOf(Student(10, "Alice"), Student(11, "Bob"))
+    val students = listOf("Alice", "Bob")
+    registerStudents(students) eq
+            listOf(Student(0, "Alice"), Student(1, "Bob"))
+    registerStudents(students, startId = 10) eq
+            listOf(Student(10, "Alice"), Student(11, "Bob"))
 }
