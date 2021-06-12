6a6efe6001b98c9fa17f354290eb55e02dfec90a
Remy Marquis <remy.marquis@gmail.com>
2021-06-12 14:26:12 +0200

    Extension Properties

--- Usability/Extension Properties/Exercise 2/src/Task.kt
+++ Usability/Extension Properties/Exercise 2/src/Task.kt
@@ -1,12 +1,12 @@
 // ExtensionProperties/Task2.kt
 package extensionPropertiesExercise2
+
 import atomictest.eq
 
-// TODO: implement 'reversed'
+val <T> List<T>.reversed: List<T>
+    get() = reversed()
 
 fun main() {
-/*
-  val list = listOf(1, 2, 3)
-  list.reversed eq listOf(3, 2, 1)
-*/
+    val list = listOf(1, 2, 3)
+    list.reversed eq listOf(3, 2, 1)
 }
