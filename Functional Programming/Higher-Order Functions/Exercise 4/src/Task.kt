ca91b7778c2ce26bf78fed3c5d4e916da2e8c00b
Remy Marquis <remy.marquis@gmail.com>
2021-06-16 16:25:51 +0200

    Higher-Order Functions

--- Functional Programming/Higher-Order Functions/Exercise 4/src/Task.kt
+++ Functional Programming/Higher-Order Functions/Exercise 4/src/Task.kt
@@ -2,14 +2,14 @@
 package higherOrderFunctionsExercise4
 
 fun <A, B, C> ((A) -> B).andThen(action: (B) -> C): (A) -> C =
-  { a: A -> TODO() }
+    { a: A -> action(this(a)) }
 
 fun main() {
-  val multiply2: (Int) -> Int = { x: Int -> x * 2 }
-  val println: (Int) -> Unit = { i: Int -> println("Result: $i") }
+    val multiply2: (Int) -> Int = { x: Int -> x * 2 }
+    val println: (Int) -> Unit = { i: Int -> println("Result: $i") }
 
-  val composition = multiply2.andThen(println)
-  composition(5)
+    val composition = multiply2.andThen(println)
+    composition(5)
 }
 /* Output:
 Result: 10
