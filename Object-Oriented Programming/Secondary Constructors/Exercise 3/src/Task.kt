2bc4b6ae31a45086a26c1982177e451fa8ec7d4d
Remy Marquis <remy.marquis@gmail.com>
2021-06-28 22:13:09 +0200

    Secondary Contructors

--- Object-Oriented Programming/Secondary Constructors/Exercise 3/src/Task.kt
+++ Object-Oriented Programming/Secondary Constructors/Exercise 3/src/Task.kt
@@ -1,35 +1,25 @@
 // SecondaryConstructors/Task3.kt
 package secondaryConstructorsExercise3
+
 import atomictest.eq
 import secondaryConstructorsExercise3.Material.*
 
 enum class Material {
-  Ceramic, Metal, Plastic
+    Ceramic, Metal, Plastic
 }
 
 class GardenItem(
-    val name: String
+    val name: String = "Strange Thing",
+    val material: Material = Plastic
 ) {
-  var material: Material = Plastic
-
-  constructor(
-      name: String, material: Material
-  ) : this(name) {
-    this.material = material
-  }
-
-  constructor(
-      material: Material
-  ) : this("Strange Thing", material)
-
-  override fun toString() = "$material $name"
+    override fun toString() = "$material $name"
 }
 
 fun main() {
-  GardenItem("Elf").material eq Plastic
-  GardenItem("Snowman").name eq "Snowman"
-  GardenItem("Gazing Ball", Metal) eq
-    "Metal Gazing Ball"
-  GardenItem(material = Ceramic) eq
-    "Ceramic Strange Thing"
+    GardenItem("Elf").material eq Plastic
+    GardenItem("Snowman").name eq "Snowman"
+    GardenItem("Gazing Ball", Metal) eq
+            "Metal Gazing Ball"
+    GardenItem(material = Ceramic) eq
+            "Ceramic Strange Thing"
 }
