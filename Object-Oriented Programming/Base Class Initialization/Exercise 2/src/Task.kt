4ccd21de70bda04920e3c2d51de01eb4d0f11305
Remy Marquis <remy.marquis@gmail.com>
2021-07-04 15:19:27 +0200

    Base Class Initialization

--- Object-Oriented Programming/Base Class Initialization/Exercise 2/src/Task.kt
+++ Object-Oriented Programming/Base Class Initialization/Exercise 2/src/Task.kt
@@ -1,19 +1,20 @@
 // BaseClassInit/Task2.kt
 package baseClassInitializationExercise2
+
 import atomictest.trace
 
 open class Plate(description: String) {
-  init {
-    trace("Plate-$description")
-  }
+    init {
+        trace("Plate-$description")
+    }
 }
 
 class DinnerPlate : Plate("DinnerPlate")
 
 open class Utensil(description: String) {
-  init {
-    trace("Utensil-$description")
-  }
+    init {
+        trace("Utensil-$description")
+    }
 }
 
 class Spoon : Utensil("Spoon")
@@ -23,16 +24,25 @@ class Fork : Utensil("Fork")
 class Knife : Utensil("Knife")
 
 open class Custom {
-  init {
-    trace("Custom")
-  }
+    init {
+        trace("Custom")
+    }
 }
 
-class PlaceSetting
+class PlaceSetting : Custom() {
+    val spoon = Spoon()
+    val fork = Fork()
+    val knife = Knife()
+    val plate = DinnerPlate()
+
+    init {
+        trace("PlaceSetting")
+    }
+}
 
 fun main() {
-  PlaceSetting()
-  trace eq """
+    PlaceSetting()
+    trace eq """
     Custom
     Utensil-Spoon
     Utensil-Fork
--- Object-Oriented Programming/Base Class Initialization/Exercise 3/src/Task.kt
+++ Object-Oriented Programming/Base Class Initialization/Exercise 3/src/Task.kt
@@ -1,17 +1,20 @@
 // BaseClassInit/Task3.kt
 package baseClassInitializationExercise3
+
 import atomictest.trace
 
-/* TODO: Implement Animal, Cat and Dog classes */
+open class Animal(val sound: String = "???")
+
+class Cat(sound: String = "meow") : Animal(sound)
+
+class Dog(sound: String = "woof") : Animal(sound)
 
 fun main() {
-/*
-  val animal = Animal()
-  trace(animal.sound)
-  val cat = Cat()
-  trace(cat.sound)
-  val dog = Dog()
-  trace(dog.sound)
-*/
-  trace eq "??? meow woof"
+    val animal = Animal()
+    trace(animal.sound)
+    val cat = Cat()
+    trace(cat.sound)
+    val dog = Dog()
+    trace(dog.sound)
+    trace eq "??? meow woof"
 }
