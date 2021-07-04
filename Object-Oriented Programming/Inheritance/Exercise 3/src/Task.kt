3d1d030275539c157615761a8d1af1089ddc426b
Remy Marquis <remy.marquis@gmail.com>
2021-07-04 14:44:20 +0200

    Inheritance

--- Object-Oriented Programming/Inheritance/Exercise 3/src/Task.kt
+++ Object-Oriented Programming/Inheritance/Exercise 3/src/Task.kt
@@ -1,19 +1,28 @@
 // Inheritance/Task3.kt
 package inheritanceExercise3
+
 import atomictest.eq
 
 open class Game {
-  // TODO
+    val initOrder = mutableListOf<String>()
+
+    init {
+        initOrder += "Game"
+    }
 }
 
-open class BoardGame: Game() {
-  // TODO
+open class BoardGame : Game() {
+    init {
+        initOrder += "BoardGame"
+    }
 }
 
-class Chess: BoardGame() {
-  // TODO
+class Chess : BoardGame() {
+    init {
+        initOrder += "Chess"
+    }
 }
 
 fun main() {
-  // Chess().initOrder eq "[Game, BoardGame, Chess]"
+    Chess().initOrder eq "[Game, BoardGame, Chess]"
 }
