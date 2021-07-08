7d05c9904c95ff591f82b3db8622dec125cc432d
Remy Marquis <remy.marquis@gmail.com>
2021-07-08 18:48:11 +0200

    Polymorphism

--- Object-Oriented Programming/Polymorphism/Exercise 1/src/Task.kt
+++ Object-Oriented Programming/Polymorphism/Exercise 1/src/Task.kt
@@ -1,54 +1,59 @@
 // Polymorphism/Task1.kt
 package polymorphismExercise1
+
 import atomictest.*
 
 abstract class Character(val name: String) {
-  abstract fun play(): String
+    abstract fun play(): String
 }
 
 interface Fighter {
-  fun fight() = "Fight!"
+    fun fight() = "Fight!"
 }
 
 interface Magician {
-  fun doMagic() = "Magic!"
+    fun doMagic() = "Magic!"
 }
 
 interface Flyer {
-  fun fly() = "Fly!"
+    fun fly() = "Fly!"
 }
 
 class Warrior :
-  Character("Warrior"), Fighter {
-  override fun play() = fight()
+    Character("Warrior"), Fighter {
+    override fun play() = fight()
 }
 
 open class Elf(name: String = "Elf") :
-  Character(name), Magician {
-  override fun play() = doMagic()
+    Character(name), Magician {
+    override fun play() = doMagic()
 }
 
 class FightingElf :
-  Elf("FightingElf"), Fighter {
-  override fun play() =
-    super.play() + fight()
+    Elf("FightingElf"), Fighter {
+    override fun play() =
+        super.play() + fight()
 }
 
-class Dragon
+class Dragon : Character("Dragon"), Flyer {
+    override fun play() = fly()
+}
 
-class Wizard
+class Wizard : Character("Magician"), Magician, Flyer {
+    override fun play() = doMagic() + fly()
+}
 
 fun Character.playTurn() = name + ": " + play()
 
 fun main() {
-  listOf(
-    Warrior(),
-    Elf(),
-    FightingElf(),
-    Dragon(),
-    Wizard()
-  ).map { TODO() } eq
-    "[Warrior: Fight!, Elf: Magic!, " +
-    "FightingElf: Magic!Fight!, " +
-    "Dragon: Fly!, Magician: Magic!Fly!]"
+    listOf(
+        Warrior(),
+        Elf(),
+        FightingElf(),
+        Dragon(),
+        Wizard()
+    ).map { it.playTurn() } eq
+            "[Warrior: Fight!, Elf: Magic!, " +
+            "FightingElf: Magic!Fight!, " +
+            "Dragon: Fly!, Magician: Magic!Fly!]"
 }
--- Object-Oriented Programming/Polymorphism/Exercise 3/src/Task.kt
+++ Object-Oriented Programming/Polymorphism/Exercise 3/src/Task.kt
@@ -2,19 +2,41 @@
 package polymorphismExercise3
 import atomictest.trace
 
-// TODO
+interface Connection {
+    fun send(message: String)
+}
+
+class Text : Connection {
+    override fun send(message: String) =
+        trace("'$message' via Text")
+}
+
+class Phone : Connection {
+    override fun send(message: String) =
+        trace("'$message' via Phone")
+}
+
+class Email : Connection {
+    override fun send(message: String) =
+        trace("'$message' via Email")
+}
+
+class Message(
+    val text: String,
+    val connection: Connection
+) {
+    fun transmit() = connection.send(text)
+}
 
 fun main() {
-/*
-  listOf(
-    Message("We", Text()),
-    Message("Are", Phone()),
-    Message("Fine", Email())
-  ).forEach { it.transmit() }
-  trace eq """
+    listOf(
+        Message("We", Text()),
+        Message("Are", Phone()),
+        Message("Fine", Email())
+    ).forEach { it.transmit() }
+    trace eq """
     'We' via Text
     'Are' via Phone
     'Fine' via Email
   """
-*/
 }
