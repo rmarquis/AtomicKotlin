66ca3806d44f910f3986d452487bcdf20bb37920
Remy Marquis <remy.marquis@gmail.com>
2021-07-09 21:15:41 +0200

    Composition

--- Object-Oriented Programming/Composition/Exercise 3/src/Task.kt
+++ Object-Oriented Programming/Composition/Exercise 3/src/Task.kt
@@ -1,19 +1,20 @@
 // Composition/Task3.kt
 package compositionExercise3
+
 import atomictest.*
 
 class Queue<E> {
-  // private val arrayDeque
-  fun put(e: E) = "TODO"
-  fun poll(): E = TODO()
+    private val arrayDeque = ArrayDeque<E>()
+    fun put(e: E) = arrayDeque.addLast(e)
+    fun poll(): E = arrayDeque.removeFirst()
 }
 
 fun main() {
-  val queue = Queue<String>()
-  queue.put("a")
-  queue.put("b")
-  queue.put("c")
-  queue.put("d")
-  queue.poll() eq "a"
-  queue.poll() eq "b"
+    val queue = Queue<String>()
+    queue.put("a")
+    queue.put("b")
+    queue.put("c")
+    queue.put("d")
+    queue.poll() eq "a"
+    queue.poll() eq "b"
 }
