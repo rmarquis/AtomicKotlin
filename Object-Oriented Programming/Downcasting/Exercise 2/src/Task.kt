75ba158dd74031c3f95dc80eb5af1f95097c5ca8
Remy Marquis <remy.marquis@gmail.com>
2021-07-10 23:04:37 +0200

    Downcasting

--- Object-Oriented Programming/Downcasting/Exercise 2/src/Task.kt
+++ Object-Oriented Programming/Downcasting/Exercise 2/src/Task.kt
@@ -1,6 +1,16 @@
 // DownCasting/DownCastEx2.kt
 package downcastingExercise2
+
 import downcasting.*
 import atomictest.eq
 
-// TODO
+val group: List<Creature> = listOf(
+    Human(), Human(), Dog(), Alien(), Dog()
+)
+
+fun main() {
+    group.filterIsInstance<Dog>()
+        .map(Dog::bark) eq "[Yip!, Yip!]"
+    group.filterIsInstance(Dog::class.java)
+        .map(Dog::bark) eq "[Yip!, Yip!]"
+}
