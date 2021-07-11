59cd3e5b2248ed5ec6d49c2f113a61dce830daa0
Remy Marquis <remy.marquis@gmail.com>
2021-07-11 12:58:23 +0200

    Sealed Classes

--- Object-Oriented Programming/Sealed Classes/Exercise 2/src/Task.kt
+++ Object-Oriented Programming/Sealed Classes/Exercise 2/src/Task.kt
@@ -1,6 +1,17 @@
 // SealedClasses/SealedEx2.kt
 package sealedClassesExercise2
+
 import sealedClassesExercise1.*
 import atomictest.*
 
-// TODO
+fun main() {
+    Transport::class.sealedSubclasses
+        .map { it.simpleName }
+        .forEach { trace(it) }
+    trace eq """
+    Train
+    Bus
+    Tram
+    Plane
+  """
+}
