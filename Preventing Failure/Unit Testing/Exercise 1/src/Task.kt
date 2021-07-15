b4ad4a529a84b7fbd7a09480850c8eedc8e46581
Remy Marquis <remy.marquis@gmail.com>
2021-07-15 15:17:37 +0200

    Unit Testing

--- Preventing Failure/Unit Testing/Exercise 1/src/Task.kt
+++ Preventing Failure/Unit Testing/Exercise 1/src/Task.kt
@@ -1,8 +1,19 @@
 // Tests/unittesting/UnitTestingSoln1.kt
 package unitTestingExercise1
+
 import sealedclasses.Bus
 import sealedclasses.Train
 import sealedclasses.travel
 import kotlin.test.*
 
-// TODO
+class SealedClassesTest {
+    @Test
+    fun `train travel`() {
+        assertEquals("Train S1", travel(Train("S1")))
+    }
+
+    @Test
+    fun `bus travel`() {
+        assertEquals("Bus 11: size 90", travel(Bus("11", 90)))
+    }
+}
