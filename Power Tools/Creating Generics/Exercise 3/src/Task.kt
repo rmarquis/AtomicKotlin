86c6e236d8a05390da3f97ad7d067c86ef29d9f7
Remy Marquis <remy.marquis@gmail.com>
2021-07-16 10:09:46 +0200

    Creating Generics

--- Power Tools/Creating Generics/Exercise 3/src/Task.kt
+++ Power Tools/Creating Generics/Exercise 3/src/Task.kt
@@ -1,14 +1,30 @@
 // CreatingGenerics/CreatingGenericsSoln3.kt
 package creatingGenericsExercise3
+
 import variance.Cat
 
-// TODO
+interface InBox<in T> {
+    fun put(item: T)
+}
+
+interface OutBox<out T> {
+    fun get(): T
+}
+
+class Box<T>(private var contents: T) :
+    InBox<T>, OutBox<T> {
+    override fun put(item: T) {
+        contents = item
+    }
+
+    override fun get(): T = contents
+}
 
 fun main() {
-/*
-  val cg: Crate<Grape> = Crate(Grape())
-  val oc: OutCrate<Can> = cg
-  val cc: Crate<Can> = Crate(Can())
-  val ic: InCrate<Grape> = cc
-*/
+    val catBox: Box<Cat> = Box(Cat())
+    val outBoxAny: OutBox<Any> = catBox
+    val a: Any = outBoxAny.get()
+    val inBoxAny: InBox<Any> = Box(217)
+    val inBoxCat: InBox<Any> = inBoxAny
+    inBoxCat.put(Cat())
 }
