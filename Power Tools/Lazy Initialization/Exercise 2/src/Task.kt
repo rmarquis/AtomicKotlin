f8fc40e04baa7b25042f7adfc5c98b48b2e5bf92
Remy Marquis <remy.marquis@gmail.com>
2021-07-16 17:15:52 +0200

    Lazy Initialization

--- Power Tools/Lazy Initialization/Exercise 2/src/Task.kt
+++ Power Tools/Lazy Initialization/Exercise 2/src/Task.kt
@@ -1,25 +1,44 @@
 // LazyInitialization/LazyInitSoln2.kt
 package lazyInitializationExercise2
+
 import atomictest.*
 
 class Outer {
-  val s1: String = "TODO"
-  val s2: String = "TODO"
-  inner class Inner {
-    val si: String = "TODO"
-  }
+    init {
+        trace("Outer constructor")
+    }
+
+    val s1: String by lazy {
+        trace("Initializing Outer.s1")
+        "Hi"
+    }
+    val s2: String by lazy {
+        trace("Initializing Outer.s2")
+        "Hello $s1"
+    }
+
+    inner class Inner {
+        init {
+            trace("Inner constructor")
+        }
+
+        val si: String by lazy {
+            trace("Initializing Inner.si")
+            "Howdy $s2"
+        }
+    }
 }
 
 fun main() {
-  val o = Outer()
-  trace("Outer created")
-  val inner = o.Inner()
-  trace("Inner created")
-  trace("First access: inner.si:")
-  trace(inner.si)
-  trace("Second access: inner.si:")
-  trace(inner.si)
-  trace eq """
+    val o = Outer()
+    trace("Outer created")
+    val inner = o.Inner()
+    trace("Inner created")
+    trace("First access: inner.si:")
+    trace(inner.si)
+    trace("Second access: inner.si:")
+    trace(inner.si)
+    trace eq """
     Outer constructor
     Outer created
     Inner constructor
--- Power Tools/Lazy Initialization/Exercise 3/src/Task.kt
+++ Power Tools/Lazy Initialization/Exercise 3/src/Task.kt
@@ -1,29 +1,46 @@
 // LazyInitialization/LazyInitSoln3.kt
 package lazyInitializationExercise3
+
 import atomictest.*
 
 class A {
-  init {
-    // TODO
-  }
-  val b = B() // TODO
-  val i = 1 // TODO
-  fun f() = 0 // TODO
+    init {
+        trace("A constructor")
+    }
+
+    val b by lazy {
+        trace("Initializing A.b")
+        B()
+    }
+    val i by lazy {
+        trace("Initializing A.i")
+        1
+    }
+
+    fun f() = i + b.g()
 }
 
 class B {
-  init {
-    // TODO
-  }
-  val j = 2 // TODO
-  fun g() = 0 // TODO
+    init {
+        trace("B constructor")
+    }
+
+    val j by lazy {
+        trace("Initializing B.j")
+        2
+    }
+
+    fun g() = j
 }
 
 fun main() {
-  val a = A() // TODO
-  trace("'a' defined")
-  trace(a.f())
-  trace eq """
+    val a by lazy {
+        trace("Initializing A")
+        A()
+    }
+    trace("'a' defined")
+    trace(a.f())
+    trace eq """
     'a' defined
     Initializing A
     A constructor
