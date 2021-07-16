5394b121967270da0061e7c7be02a5ceacb5855a
Remy Marquis <remy.marquis@gmail.com>
2021-07-16 09:37:23 +0200

    Extension Lambdas

--- Power Tools/Extension Lambdas/Exercise 1/src/Task.kt
+++ Power Tools/Extension Lambdas/Exercise 1/src/Task.kt
@@ -1,44 +1,51 @@
 // ExtensionLambdas/ExtensionLambdasSoln1.kt
 package extensionLambdasExercise1
+
 import atomictest.eq
 
 fun createString(
-  build: StringBuilder.() -> Unit
+    build: StringBuilder.() -> Unit
 ): String {
-  TODO()
+    val sb = StringBuilder()
+    sb.build()
+    return sb.toString()
 }
 
 fun <T> createList(
-  build: ArrayList<T>.() -> Unit
+    build: ArrayList<T>.() -> Unit
 ): List<T> {
-  TODO()
+    val list = ArrayList<T>()
+    list.build()
+    return list
 }
 
 fun <K, V> createMap(
-  build: HashMap<K, V>.() -> Unit
+    build: HashMap<K, V>.() -> Unit
 ): Map<K, V> {
-  TODO()
+    val map = HashMap<K, V>()
+    map.build()
+    return map
 }
 
 @OptIn(ExperimentalStdlibApi::class)
 fun main() {
-  createString {
-    (1..10).forEach { append(it) }
-  } eq buildString {
-    (1..10).forEach { append(it) }
-  }
-  createList<String> {
-    (1..10).forEach { add(it.toString()) }
-  } eq buildList {
-    (1..10).forEach { add(it.toString()) }
-  }
-  createMap<Char, Int> {
-    ('a'..'j').forEachIndexed { n, c ->
-      put(c, n)
+    createString {
+        (1..10).forEach { append(it) }
+    } eq buildString {
+        (1..10).forEach { append(it) }
     }
-  } eq buildMap {
-    ('a'..'j').forEachIndexed { n, c ->
-      put(c, n)
+    createList<String> {
+        (1..10).forEach { add(it.toString()) }
+    } eq buildList {
+        (1..10).forEach { add(it.toString()) }
     }
-  }
+    createMap<Char, Int> {
+        ('a'..'j').forEachIndexed { n, c ->
+            put(c, n)
+        }
+    } eq buildMap {
+        ('a'..'j').forEachIndexed { n, c ->
+            put(c, n)
+        }
+    }
 }
--- Power Tools/Extension Lambdas/Exercise 3/src/Task.kt
+++ Power Tools/Extension Lambdas/Exercise 3/src/Task.kt
@@ -1,49 +1,74 @@
 // ExtensionLambdas/ExtensionLambdasSoln3.kt
 package extensionLambdasExercise3
+
 import atomictest.eq
 
 open class Cleanser : ArrayList<String>() {
-  fun selectContainer() {
-    add("Container selected")
-  }
-  fun sealContainer() {
-    add("Container sealed")
-  }
-  fun detergent() { add("detergent") }
-  fun abrasive() { add("abrasive") }
-  fun ammonia() { add("ammonia") }
-  fun water() { add("water") }
+    fun selectContainer() {
+        add("Container selected")
+    }
+
+    fun sealContainer() {
+        add("Container sealed")
+    }
+
+    fun detergent() {
+        add("detergent")
+    }
+
+    fun abrasive() {
+        add("abrasive")
+    }
+
+    fun ammonia() {
+        add("ammonia")
+    }
+
+    fun water() {
+        add("water")
+    }
 }
 
 class SprayCleanser : Cleanser() {
-  fun selectSprayer() {
-    add("Sprayer selected")
-  }
+    fun selectSprayer() {
+        add("Sprayer selected")
+    }
 }
 
-fun cleanser(): Cleanser {
-  TODO()
+fun cleanser(
+    formula: Cleanser.() -> Unit
+): Cleanser {
+    val result = Cleanser()
+    result.selectContainer()
+    result.formula()
+    result.sealContainer()
+    return result
 }
 
-fun sprayCleanser(): SprayCleanser {
-  TODO()
+fun sprayCleanser(
+    formula: SprayCleanser.() -> Unit
+): SprayCleanser {
+    val result = SprayCleanser()
+    result.selectContainer()
+    result.selectSprayer()
+    result.formula()
+    result.sealContainer()
+    return result
 }
 
 fun main() {
-/*
-  val c: Cleanser = cleanser {
-    detergent()
-    abrasive()
-  }
-  c eq "[Container selected, detergent, " +
-    "abrasive, Container sealed]"
-  val sc: SprayCleanser = sprayCleanser {
-    detergent()
-    ammonia()
-    water()
-  }
-  sc eq "[Container selected, " +
-    "Sprayer selected, detergent, " +
-    "ammonia, water, Container sealed]"
-*/
+    val c: Cleanser = cleanser {
+        detergent()
+        abrasive()
+    }
+    c eq "[Container selected, detergent, " +
+            "abrasive, Container sealed]"
+    val sc: SprayCleanser = sprayCleanser {
+        detergent()
+        ammonia()
+        water()
+    }
+    sc eq "[Container selected, " +
+            "Sprayer selected, detergent, " +
+            "ammonia, water, Container sealed]"
 }
